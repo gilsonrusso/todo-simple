b9adc4dab127740e829022461fc6c0720850e358
Gilson Russo <gilsonrusso@outlook.com>
2023-04-23 13:32:48 -0400

    Add intial global style with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { themeDefault } from "./styles/themes/default";
 
 function App() {
 
   return (
-    <h1>Hello</h1>
+    <ThemeProvider theme={themeDefault}>
+      <h1>Hello</h1>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
