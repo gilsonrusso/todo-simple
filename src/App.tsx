b7551bd325188548320fb4b684a7b288e6984249
Gilson Russo <gilsonrusso@outlook.com>
2023-04-23 15:40:01 -0400

    Add router to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { themeDefault } from "./styles/themes/default";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
 
   return (
     <ThemeProvider theme={themeDefault}>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
